3d097897eba375ec6901422d5dc96a9b61018137
JituRaj007 <jitendragupta.xhtmlchop@gmail.com>
2023-06-06 20:39:55 +0530

    updated Last

--- components/Home/Banner/index.tsx
+++ components/Home/Banner/index.tsx
@@ -29,5 +29,5 @@ const Banner = () => {
             </div>
         </>
     )
-};
+}; 
 export default Banner;
--- components/Home/Digitalmarketing/index.tsx
+++ components/Home/Digitalmarketing/index.tsx
@@ -27,5 +27,4 @@ const Digitalmarketing = () =>{
         </>
     )
 };
-
 export default Digitalmarketing;
